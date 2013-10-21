b57b867e8e54de62417c8222d64ecce91a235ef8
Dag Rende <dag.rende@find-out.se>
2013-10-21 08:52:56 +0200

    makes 4 walls with rounded top and corners

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,7 +1,8 @@
 function Contr($scope) {
-	  $scope.buildingpos = new THREE.Vector3(1, 0, 0);
-	  
-	  $scope.config = {
-			  size: '4,1,3'
-	  };
+	$scope.buildingpos = new THREE.Vector3(1, 0, 0);
+
+	$scope.config = {
+		size : '50,10,30',
+		wallThickness: 1
+	};
 }
